9d41bbdc29b8218261a22ded59ea84f877660b9e
MdMizanurRahman1 <mizan.rahman2217@gmail.com>
2023-01-26 23:34:08 +0200

    maths basic

--- first.js
+++ first.js
@@ -1,1 +1,24 @@
 console.log(21 + 2);
+var tablet = "mizan";
+var phone = "rahman";
+
+var tabletPhone = tablet + " " + phone;
+console.log(tabletPhone);
+
+var nme = 21;
+var surName = "42";
+surName = parseInt(surName);
+var public = nme + surName;
+//console.log(public);
+//console.log = parseInt(public);
+
+var gpa = '3.42';
+var gpaNumber = parseFloat(gpa);
+console.log(gpaNumber);
+
+var cgpa = 3.99294;
+//cgpa = toFixed(2);
+//console.log(cgpa);
+//console.log(typeof cgpa);
+cgpa = cgpa.toFixed(2);
+console.log(cgpa);
